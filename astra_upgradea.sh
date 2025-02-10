5bba9f431192dd009631565a9f2822cfda324e2d
warliker99 <71266150+warliker99@users.noreply.github.com>
2025-02-10 16:07:54 +0300

    Update astra_upgradea.sh

--- astra_upgradea.sh
+++ astra_upgradea.sh
@@ -11,7 +11,7 @@ ASTRA_BUILD_VERSION_SRC=`cat /etc/astra/build_version | head -n 1 | awk -F"." '{
 if [ASTRA_VERSION_DST != ASTRA_VERSION_SRC || ASTRA_BUILD_VERSION_DST != ASTRA_BUILD_VERSION_SRC]; then
   #Добавление репозиториев Astra Linux
   cat <<EOL > /etc/apt/sources.list
-  deb  1.8_x86-64 contrib main non-free non-free-firmware
+  deb $ASTRA_BASE 1.8_x86-64 contrib main non-free non-free-firmware
   EOL
   
   #Обновление ОС
@@ -29,5 +29,10 @@ if [ASTRA_VERSION_DST != ASTRA_VERSION_SRC || ASTRA_BUILD_VERSION_DST != ASTRA_B
 
 else
   echo "ОС уже была обновлена.\nАктуальная версия: $ASTRA_VERSION_SRC\nАктуальный build: $ASTRA_BUILD_VERSION_SRC"
+  cat <<EOL > /etc/apt/sources.list
+  deb $ASTRA_BASE 1.8_x86-64 contrib main non-free non-free-firmware
+  deb $ASTRA_EXT 1.8_x86-64 contrib main non-free non-free-firmware
+  EOL
+  apt update -y
   exit 0
 fi
