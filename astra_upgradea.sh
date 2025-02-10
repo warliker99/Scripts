ff8be441e0314977aa7ed6864c4bb96fe52fde99
warliker99 <71266150+warliker99@users.noreply.github.com>
2025-02-10 15:50:41 +0300

    Update astra_upgradea.sh

--- astra_upgradea.sh
+++ astra_upgradea.sh
@@ -9,16 +9,27 @@ ASTRA_BUILD_VERSION_SRC=`cat /etc/astra/build_version | awk -F"." '{print $1}`
 #**************************************************************************************************************************
 
 if [ASTRA_VERSION_DST != ASTRA_VERSION_SRC || ASTRA_BUILD_VERSION_DST != ASTRA_BUILD_VERSION_SRC]; then
+  #Добавление репозиториев Astra Linux
+  cat <<EOL > /etc/apt/sources.list
+  deb  1.8_x86-64 contrib main non-free non-free-firmware
+  EOL
+  
+  #Обновление ОС
+  apt update -y
+  apt install astra-update -y
+  astra-update -A -r -T
+  
+  ASTRA_BUILD_VERSION_SRC_POST=`cat /etc/astra/build_version | awk -F"." '{print $1}`
+  
+  if [ ASTRA_BUILD_VERSION_DST == ASTRA_BUILD_VERSION_SRC]; then
+    echo "ОС успешно обновлена."
+  else
+    echo "Ошибка обновления!\nПроверьте логи обновления на наличие ошибок: /var/log/astra_update*.log"
+    exit 1
+  
 
 
-
-#Добавление репозиториев Astra Linux
-cat <<EOL > /etc/apt/sources.list
-deb  1.8_x86-64 contrib main non-free non-free-firmware
-EOL
-
-#Обновление ОС
-apt update -y
-apt install astra-update -y
-astra-update -A -r -T
+else
+  echo "ОС уже была обновлена.\nАктуальная версия: $ASTRA_VERSION_SRC\nАктуальный build: $ASTRA_BUILD_VERSION_SRC"
+  exit0
 fi
